563aa21f0017d5a73e35df08903ce71de4e186ce
Frederick Morrison <frederick.morrison@outlook.com>
2024-09-23 16:14:27 -0600

    adjust unit tests

--- tests/UnitTests.java
+++ tests/UnitTests.java
@@ -2,9 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-//import  org.junit.jupiter.api.Assertions.assertTrue;
-
-
 public class UnitTests {
 
     @Test
